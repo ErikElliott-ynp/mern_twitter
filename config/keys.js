a9cd9f78721e6ec9889bb1dafc90b500e08b7c8a
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-16 23:11:38 -0800

    Add files

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dev:SKlTtzESRIUexNQx@cluster0-hkaji.mongodb.net/test?retryWrites=true&w=majority',
+    //Make sure this is your own unique string
+    secretOrKey: '9FbiDOSYeUe.d+|B]=b[@R3n67eY3h'
+}
+
---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import errors from "./errors_reducer";
+import { combineReducers } from 'redux';
+import session from './session_reducer';
+
+const RootReducer = combineReducers({
+    session,
+    errors
+});
+
+export default RootReducer;
