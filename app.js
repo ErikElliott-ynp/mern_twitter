f04c681669f9e0fca13a05931f9376497e627a25
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-16 16:21:26 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const mongoose =  require('mongoose');
+const db = require('./config/keys').mongoURI;
+mongoose 
+    .connect(db, { useNewUrlParser: true})
+    .then(() => console.log("Connected to MongDB so smooth"))
+    .catch( err => console.log(err));
+
+
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => res.send("??????"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on ${port}` ));
